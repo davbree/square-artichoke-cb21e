c3f536e01cfd39062df6beb2227fb40faff83651
Stackbit Code Editor <projects@stackbit.com>
2021-11-23 10:08:01 +0000

    register-components.js: changed

--- src/components/register-components.js
+++ src/components/register-components.js
@@ -1,11 +1,12 @@
 import dynamic from 'next/dynamic';
 import { registerComponents } from '@stackbit/components';
 import { componentsMap } from '@stackbit/components/dist/components-map';
-import Header from "./"
+import Header from "./Header.tsx";
+
 registerComponents({
     // Register all Stackbit components
     ...componentsMap,
-    Header: 
+    Header
 
     // Override any static or dynamic component,
     // or register your own dynamic component.
