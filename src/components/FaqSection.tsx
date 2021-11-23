2c956940b75fae78e87ca606b6bb2b30c4458605
Stackbit Code Editor <projects@stackbit.com>
2021-11-23 10:02:26 +0000

    FaqSection.tsx: changed

--- src/components/FaqSection.tsx
+++ src/components/FaqSection.tsx
@@ -71,6 +71,54 @@ function FaqItem() {
 
 }
 
+function ItemBlock(props) {
+    const cssId = props.elementId || null;
+    return (
+        <article id={cssId} className="sb-component sb-component-block sb-component-item" data-sb-field-path={props.annotationPrefix}>
+            {props.featuredImage && (
+                <div className="mb-4" data-sb-field-path=".featuredImage">
+                    <ImageBlock {...props.featuredImage} className="mx-auto" />
+                </div>
+            )}
+            {props.title && (
+                <h3 className={classNames(props?.styles?.title ? mapStyles(props?.styles?.title) : null)} data-sb-field-path=".title">
+                    {props.title}
+                </h3>
+            )}
+            {props.subtitle && (
+                <p
+                    className={classNames('text-lg', props?.styles?.subtitle ? mapStyles(props?.styles?.subtitle) : null, { 'mt-1': props.title })}
+                    data-sb-field-path=".subtitle"
+                >
+                    {props.subtitle}
+                </p>
+            )}
+            {props.text && (
+                <Markdown
+                    options={{ forceBlock: true, forceWrapper: true }}
+                    className={classNames('sb-markdown', props?.styles?.text ? mapStyles(props?.styles?.text) : null, {
+                        'mt-4': props.title || props.subtitle
+                    })}
+                    data-sb-field-path=".text"
+                >
+                    {props.text}
+                </Markdown>
+            )}
+            {props.author && (
+                <div className={classNames('text-sm', { 'mt-4': props.title || props.subtitle || props.text })} data-sb-field-path=".author">
+                    {props.author}
+                </div>
+            )}
+            {props.isRatingVisible && props.rating && (
+                <div className={classNames({ 'mt-4': props.title || props.subtitle || props.text || props.author })} data-sb-field-path=".rating">
+                    {props.rating}
+                </div>
+            )}
+            {itemActions(props)}
+        </article>
+    );
+}
+
 function mapColStyles(columns) {
     switch (columns) {
         case 4:
